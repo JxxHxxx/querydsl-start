6fb438183fcea8ed4e86f5f67487bc978fce3bb9
JxxHxxx <leesin5498@naver.com>
2023-05-12 16:16:29 +0900

    프로젝트 설정 및 예제 도메인

---
+++ src/test/java/com/jxx/querydslstart/QuerydslStartApplicationTests.java
@@ -0,0 +1,35 @@
+package com.jxx.querydslstart;
+
+import com.jxx.querydslstart.entity.Hello;
+import com.jxx.querydslstart.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class QuerydslStartApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = new QHello("h");
+
+        Hello result = query.selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
+}
